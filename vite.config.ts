ff9e3788a20e4ff2d4795f24c00f69917f905b75
shopifydev-ns <shopifydev@gmail.com>
2026-03-24 15:56:56 +0200

    chore: Reverce vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,7 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-
-
-// import { defineConfig } from 'vite'
-
-// export default defineConfig({
-//   base: '/coach-pilot/',
-// })
-
-
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
-  base: '/coach-pilot/',
-  build: {
-    outDir: 'docs'
-  }
+  plugins: [react()],
 })
